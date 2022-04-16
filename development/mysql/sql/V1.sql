c46425dd57b2d7d10f352109d66b500e718a4660
Mamoru Saita <mamosaita64@gmail.com>
2022-04-17 00:21:53 +0900

    first

--- development/mysql/sql/V1.sql
+++ development/mysql/sql/V1.sql
@@ -2,4 +2,22 @@ CREATE INDEX record_index
     ON record (updated_at ASC, record_id DESC);
 
 
+ALTER TABLE group_member ADD INDEX index_group_member(user_id);
+
+ALTER TABLE user ADD INDEX index_user(user_id);
+
+ALTER TABLE group_info ADD INDEX index_group_info(group_id);
+
+ALTER TABLE category ADD INDEX index_category(category_id);
+
+ALTER TABLE record_item_file ADD INDEX index_item(item_id);
+
+ALTER TABLE record_last_access ADD INDEX index_item(user_id, record_id);
+
+ALTER TABLE session ADD INDEX index_session(value);
+
+ALTER TABLE session ADD INDEX index_record_comment(linked_record_id);
+
+
+
 
